0a2b2eaefa0d7c88eab1a1118b493ddb6c38ec66
vpironski <vpironski@gmail.com>
2024-11-16 00:48:22 +0200

    Initial Commit

---
+++ src/test/java/org/cocreate/CoCreate/CoCreateApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cocreate.CoCreate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoCreateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
